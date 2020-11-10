ee3e5d63ab10d2b988c8d7f41cb3aff9b156d90b
Branford Shaw <shaw.branford@gmail.com>
2020-11-10 11:09:18 -0500

    imported jquery datepicker; had to use google cdn version of jquery instead of bootstrap's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
     $(function () {
         $("#checkInDate").datepicker({
-            showOn: 'button',
-            buttonImageOnly: true,
-            buttonImage: 'images/calendar.gif',
             dateFormat: 'dd-M-yy'
         });
     });
