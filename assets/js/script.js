da2c1a05cab7bc53bcb58b6d8e7dcffec6adede8
Branford Shaw <shaw.branford@gmail.com>
2020-11-10 10:57:22 -0500

    working on check-in boxes

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+    $(function () {
+        $("#checkInDate").datepicker({
+            showOn: 'button',
+            buttonImageOnly: true,
+            buttonImage: 'images/calendar.gif',
+            dateFormat: 'dd-M-yy'
+        });
+    });
