b7df669189652c5b386bb2cfdadc2ff1013899af
Branford Shaw <shaw.branford@gmail.com>
2020-11-10 12:40:35 -0500

    styled booking boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,15 @@
         });
     });
 
-    
+    $('.counter-btn').click(function(e) {
+        e.preventDefault();
+        var $btn = $(this);
+        $('#output-' + $btn.data('index')).html(function(i, val) {
+            val = val * 1 + $btn.data('inc');
+            return (val <= 0 ? '' : '') + val;
+        });
+    });
+
+    $(function(){
+        $('input[type="number"]').niceNumber();
+    })
